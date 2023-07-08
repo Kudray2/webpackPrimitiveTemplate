72fc50a6ba7c10221e70ff27e4600884ed72c6c8
Kudray2 <kudray@yandex.ru>
2023-07-08 20:38:17 +0300

    react 18 correction

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
-import {render} from "react-dom";
-import './index.scss'
-import {App} from "./App";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+import { App } from "./App";
 
-render (
-    <>
-        <App />
-    </>,
-    document.getElementById('root')
-)
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);
